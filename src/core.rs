8d96b39d1cb016c062091041bed4f350b6651f69
camilossantos2809 <camilossantos.2809@gmail.com>
2020-04-11 20:26:54 -0300

    refatoração

---
+++ src/core.rs
@@ -0,0 +1,91 @@
+extern crate ftp;
+extern crate unrar;
+
+use ftp::FtpStream;
+use std::fs::{create_dir_all, File};
+use std::io;
+use std::io::{Cursor, Write};
+use std::vec::Vec;
+use unrar::Archive;
+use zip::ZipArchive;
+
+pub enum TypeCompressFile {
+    Rar,
+    Zip,
+}
+
+fn write_file(file: &str, remote_file: Cursor<Vec<u8>>) {
+    println!("Transferindo dados para máquina local...");
+    // Diretório/arquivo local que será escrito com os bytes do arquivo do ftp server
+    let mut buffer = File::create(file).unwrap();
+    // Transfere os bytes da memória para a máquina local
+    buffer.write_all(&remote_file.into_inner()).unwrap();
+    println!("Arquivo gravado: {}", file);
+}
+
+fn download_ftp(dir: &str, file: &str) -> Cursor<Vec<u8>> {
+    // Conecta ao ftp server
+    let mut ftp_stream = FtpStream::connect("ftp.rpinfo.com.br:21").unwrap();
+    // Realiza login
+    let _ = ftp_stream.login("supermercado", "rpsupermercado10");
+    // Acessa o diretório
+    let _ = ftp_stream.cwd(dir);
+    // Retorna o diretório atual
+    println!(
+        "Realizando download do arquivo {}/{}...",
+        ftp_stream.pwd().unwrap(),
+        file
+    );
+    // Retorna cursor com bytes do arquivo existente no ftp server
+    let remote_file = ftp_stream.simple_retr(file).unwrap();
+    // Encerra conexão com ftp server
+    let _ = ftp_stream.quit();
+    remote_file
+}
+
+fn extract_zip_file(file_path: String, dir: &str) {
+    println!("Descompactando arquivo...");
+    let file = File::open(&file_path).unwrap();
+    let mut zip_file = ZipArchive::new(file).unwrap();
+    for i in 0..zip_file.len() {
+        let mut loop_file = zip_file.by_index(i).unwrap();
+        if loop_file.size() > 0 {
+            let file = format!("{}{}", &dir, loop_file.name());
+            let mut buffer = File::create(file).unwrap();
+            io::copy(&mut loop_file, &mut buffer).unwrap();
+        } else {
+            create_dir_all(format!(
+                "{}{}",
+                dir,
+                loop_file.sanitized_name().as_path().display()
+            ))
+            .unwrap();
+        }
+    }
+    println!("Arquivo descompactado");
+}
+
+fn extract_rar_file(file: String, dir: &str) {
+    println!("Descompactando arquivo...");
+    Archive::new(file)
+        .extract_to(dir.to_string())
+        .unwrap()
+        .process()
+        .unwrap();
+    println!("Arquivo descompactado");
+}
+
+pub fn process_upgrade(
+    dir_file: &str,
+    name_file: &str,
+    remote_dir: &str,
+    remote_name_file: &str,
+    type_compress: TypeCompressFile,
+) {
+    let remote_file = download_ftp(remote_dir, remote_name_file);
+    write_file(&name_file, remote_file);
+    match type_compress {
+        TypeCompressFile::Rar => extract_rar_file(name_file.to_string(), dir_file),
+        TypeCompressFile::Zip => extract_zip_file(name_file.to_string(), dir_file),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,4 @@
-extern crate ftp;
-extern crate unrar;
-
-use ftp::FtpStream;
-use std::fs::{create_dir_all, File};
-use std::io;
-use std::io::{Cursor, Write};
-use std::vec::Vec;
-use unrar::Archive;
-use zip::ZipArchive;
-
-fn download_ftp(dir: &str, file: &str) -> Cursor<Vec<u8>> {
-    // Conecta ao ftp server
-    let mut ftp_stream = FtpStream::connect("ftp.rpinfo.com.br:21").unwrap();
-    // Realiza login
-    let _ = ftp_stream.login("supermercado", "rpsupermercado10");
-    // Acessa o diretório
-    let _ = ftp_stream.cwd(dir);
-    // Retorna o diretório atual
-    println!(
-        "Realizando download do arquivo {}/{}...",
-        ftp_stream.pwd().unwrap(),
-        file
-    );
-    // Retorna cursor com bytes do arquivo existente no ftp server
-    let remote_file = ftp_stream.simple_retr(file).unwrap();
-    // Encerra conexão com ftp server
-    let _ = ftp_stream.quit();
-    remote_file
-}
-
-fn write_file(file: &str, remote_file: Cursor<Vec<u8>>) {
-    println!("Transferindo dados para máquina local...");
-    // Diretório/arquivo local que será escrito com os bytes do arquivo do ftp server
-    let mut buffer = File::create(file).unwrap();
-    // Transfere os bytes da memória para a máquina local
-    buffer.write_all(&remote_file.into_inner()).unwrap();
-    println!("Arquivo gravado: {}", file);
-}
-
-fn extract_zip_file(file_path: String, dir: &str) {
-    println!("Descompactando arquivo...");
-    let file = File::open(&file_path).unwrap();
-    let mut zip_file = ZipArchive::new(file).unwrap();
-    for i in 0..zip_file.len() {
-        let mut loop_file = zip_file.by_index(i).unwrap();
-        if loop_file.size() > 0 {
-            let file = format!("{}{}", &dir, loop_file.name());
-            let mut buffer = File::create(file).unwrap();
-            io::copy(&mut loop_file, &mut buffer).unwrap();
-        } else {
-            create_dir_all(format!(
-                "{}{}",
-                dir,
-                loop_file.sanitized_name().as_path().display()
-            ))
-            .unwrap();
-        }
-    }
-    println!("Arquivo descompactado");
-}
-
-fn extract_rar_file(file: String, dir: &str) {
-    println!("Descompactando arquivo...");
-    Archive::new(file)
-        .extract_to(dir.to_string())
-        .unwrap()
-        .process()
-        .unwrap();
-    println!("Arquivo descompactado");
-}
-
-enum TypeCompressFile {
-    Rar,
-    Zip,
-}
-
-fn process_upgrade(
-    dir_file: &str,
-    name_file: &str,
-    remote_dir: &str,
-    remote_name_file: &str,
-    type_compress: TypeCompressFile,
-) {
-    let remote_file = download_ftp(remote_dir, remote_name_file);
-    write_file(&name_file, remote_file);
-    match type_compress {
-        TypeCompressFile::Rar => extract_rar_file(name_file.to_string(), dir_file),
-        TypeCompressFile::Zip => extract_zip_file(name_file.to_string(), dir_file),
-    }
-}
+mod core;
 
 fn download_flex() {
     println!("Atualização do ERP Flex iniciada...");
@@ -96,12 +6,12 @@ fn download_flex() {
     let name_file = format!("{}erp-Alfa.rar", dir_file);
     let remote_dir = "install/erp/alpha_builds";
     let remote_name_file = "erp-Alfa.rar";
-    process_upgrade(
+    core::process_upgrade(
         dir_file,
         &name_file,
         remote_dir,
         remote_name_file,
-        TypeCompressFile::Rar,
+        core::TypeCompressFile::Rar,
     );
     println!("Atualização do ERP Flex concluída");
 }
@@ -113,12 +23,12 @@ fn download_rpservices() {
     let name_file = format!("{}rpservices-2.0.0.67.zip", dir_file);
     let remote_dir = "install/erp";
     let remote_name_file = "rpservices-2.0.0.67.zip";
-    process_upgrade(
+    core::process_upgrade(
         dir_file,
         &name_file,
         remote_dir,
         remote_name_file,
-        TypeCompressFile::Zip,
+        core::TypeCompressFile::Zip,
     );
     println!("Atualização do RPServices concluída");
 }
@@ -126,6 +36,6 @@ fn download_rpservices() {
 fn main() {
     println!("Iniciando procedimento...");
     download_flex();
-    download_rpservices();
+    // download_rpservices();
     println!("Procedimento finalizado!");
 }
